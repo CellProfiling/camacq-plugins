0009f5e02af544c08597f9818731597aeb321822
Martin Hjelmare <marhje52@kth.se>
2019-12-01 22:47:43 +0100

    Extend test

--- tests/production/test_production.py
+++ tests/production/test_production.py
@@ -1,4 +1,8 @@
 """Test the production plugin."""
+import tempfile
+from pathlib import Path
+from unittest.mock import call
+
 from asynctest import patch
 import pytest
 from ruamel.yaml import YAML
@@ -68,10 +72,15 @@ gain:
 @pytest.fixture(name="calc_gain")
 def calc_gain_fixture():
     """Mock calc_gain plugin function."""
-    with patch("camacq.plugins.gain.calc_gain") as mock_gain, patch(
-        "camacq.plugins.gain.make_proj"
-    ):
+    with patch("camacq.plugins.gain.calc_gain") as mock_gain:
         yield mock_gain
+
+
+@pytest.fixture(name="make_proj")
+def make_proj_fixture():
+    """Mock make_proj image function."""
+    with patch("camacq.plugins.gain.make_proj") as mock_proj:
+        yield mock_proj
 
 
 class WorkflowImageEvent(ImageEvent):
@@ -85,24 +94,38 @@ class WorkflowImageEvent(ImageEvent):
         return self.data.get("job_id")
 
 
-async def test_duplicate_image_events(center, calc_gain):
+async def test_duplicate_image_events(center, calc_gain, make_proj):
     """Test duplicate image events."""
     config = YAML(typ="safe").load(CONFIG)
     await plugins.setup_module(center, config)
+    plate_name = "00"
+    well_x = 0
+    well_y = 0
+    field_x = 1
+    field_y = 1
+    job_id = 3
+    channel_id = 31
+    save_path = Path(tempfile.gettempdir()) / plate_name
+    save_path = save_path / f"{well_x}--{well_y}"
+    test_projs = ["test"]
+    make_proj.return_value = test_projs
 
     event = WorkflowImageEvent(
         {
             "path": "test_path",
-            "plate_name": "00",
-            "well_x": 0,
-            "well_y": 0,
-            "field_x": 1,
-            "field_y": 1,
-            "job_id": 3,
-            "channel_id": 31,
+            "plate_name": plate_name,
+            "well_x": well_x,
+            "well_y": well_y,
+            "field_x": field_x,
+            "field_y": field_y,
+            "job_id": job_id,
+            "channel_id": channel_id,
         }
     )
     await center.bus.notify(event)
     await center.wait_for()
 
     assert calc_gain.call_count == 1
+    assert calc_gain.call_args == call(
+        center, config, plate_name, well_x, well_y, test_projs, True, str(save_path),
+    )
