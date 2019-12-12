0368991b1faea4a8a97217faed719b07b2cd4b7a
Martin Hjelmare <marhje52@kth.se>
2019-12-12 13:34:39 +0100

    Increase test coverage

--- tests/gain/test_gain.py
+++ tests/gain/test_gain.py
@@ -1,12 +1,12 @@
 """Test gain calculation."""
-from collections import defaultdict
+from unittest.mock import patch
 
 import numpy as np
 import pytest
 
+from camacq import plugins
 from camacq.plugins.leica import LeicaImageEvent
-from camacq.image import ImageData
-from camacqplugins.gain import GAIN_CALC_EVENT, calc_gain
+from camacqplugins.gain import GAIN_CALC_EVENT
 from tests.common import IMAGE_DATA_DIR
 
 # All test coroutines will be treated as marked.
@@ -16,23 +16,14 @@ PLATE_NAME = "slide"
 WELL_X, WELL_Y = 1, 0
 
 
-def make_proj(images):
-    """Mock make proj."""
-    sorted_images = defaultdict(list)
-    max_imgs = {}
-    for path, (channel, data) in images.items():
-        image = ImageData(path=path, data=data)
-        # Exclude images with 0, 16 or 256 pixel side.
-        # pylint: disable=len-as-condition
-        if len(image.data) == 0 or len(image.data) == 16 or len(image.data) == 256:
-            continue
-        sorted_images[channel].append(image)
-        proj = np.max([img.data for img in sorted_images[channel]], axis=0)
-        max_imgs[channel] = ImageData(path=path, data=proj)
-    return max_imgs
+@pytest.fixture(name="load_image")
+def load_image_fixture():
+    """Patch load image."""
+    with patch("camacq.image.ImageData._load_image_data", autospec=True) as load_image:
+        yield load_image
 
 
-async def test_gain(center):
+async def test_gain(center, load_image):
     """Run gain calculation test."""
     config = {
         "gain": {
@@ -62,13 +53,22 @@ async def test_gain(center):
             ],
         }
     }
+    await plugins.setup_module(center, config)
     image_fixture = IMAGE_DATA_DIR / "image_data.npz"
     image_data = await center.add_executor_job(np.load, image_fixture)
+
+    def mock_load_image(image):
+        """Mock load image."""
+        data = image_data[image.path]
+        image._data = data  # pylint: disable=protected-access
+
+    load_image.side_effect = mock_load_image
+
     events = [LeicaImageEvent({"path": path}) for path in image_data]
-    images = {
-        event.path: (event.channel_id, image_data[event.path]) for event in events
-    }
-    projs = await center.add_executor_job(make_proj, images)
+
+    for event in events:
+        await center.bus.notify(event)
+
     calculated = {}
 
     async def handle_gain_event(center, event):
@@ -83,8 +83,9 @@ async def test_gain(center):
 
     center.bus.register(GAIN_CALC_EVENT, handle_gain_event)
 
-    await calc_gain(
-        center, config, PLATE_NAME, WELL_X, WELL_Y, projs,
+    images = [event.path for event in events]
+    await center.actions.gain.calc_gain(
+        plate_name=PLATE_NAME, well_x=WELL_X, well_y=WELL_Y, images=images
     )
 
     solution = {"blue": 480, "green": 740, "red": 745, "yellow": 805}
